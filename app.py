c5f1260728ac1f5819b1a3362041ad3f4f1b5b6f
ClayGleason <clay_gleason@stu.indianhills.edu>
2025-11-10 14:22:37 -0600

    final touches

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, render_template, request, flash, redirect, url_for, session, jsonify
 from flask_login import logout_user, LoginManager, login_user, current_user, login_required
 from flask_wtf import CSRFProtect
-#from fontTools.subset import closure_glyphs
+from fontTools.subset import closure_glyphs
 from werkzeug.utils import secure_filename
 
 from APIs.api_recipes_call import load_more_recipes_with_difficulty, load_difficulties, get_all_recipes, \
@@ -130,39 +130,40 @@ def upload_page(cuisine_id):
     return render_template('game/upload.html', form=form, results=results, similarity=similarity)
 
 
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     if 'image' not in request.files:
-#         return "No file uploaded", 400
-#
-#     file = request.files['image']
-#     if file.filename == '':
-#         return "No selected file", 400
-#
-#     image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#     file.save(image_path)
-#
-#     results = analyze_image(image_path)
-#
-#     return render_template('result.html', results=results, filename=file.filename)
-#
-# @app.route('/upload')
-# def upload():
-#     return render_template('game/uploadtest.html')
-# @app.route('/compare', methods=['POST'])
-# def compare():
-#     img1 = request.files['image1']
-#     img2 = request.files['image2']
-#
-#     path1 = os.path.join(app.config['UPLOAD_FOLDER'], img1.filename)
-#     path2 = os.path.join(app.config['UPLOAD_FOLDER'], img2.filename)
-#
-#     img1.save(path1)
-#     img2.save(path2)
-#
-#     similarity = compare_images(path1, path2)
-#
-#     return render_template('result.html', similarity=similarity)
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    if 'image' not in request.files:
+        return "No file uploaded", 400
+
+    file = request.files['image']
+    if file.filename == '':
+        return "No selected file", 400
+
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(image_path)
+
+    results = analyze_image(image_path)
+
+    return render_template('result.html', results=results, filename=file.filename)
+
+@app.route('/upload')
+def upload():
+    return render_template('game/uploadtest.html')
+
+@app.route('/compare', methods=['POST'])
+def compare():
+    img1 = request.files['image1']
+    img2 = request.files['image2']
+
+    path1 = os.path.join(app.config['UPLOAD_FOLDER'], img1.filename)
+    path2 = os.path.join(app.config['UPLOAD_FOLDER'], img2.filename)
+
+    img1.save(path1)
+    img2.save(path2)
+
+    similarity = compare_images(path1, path2)
+
+    return render_template('result.html', similarity=similarity)
 
 @csrf.exempt
 @app.route('/save-avatar', methods=['POST'])
@@ -240,7 +241,6 @@ def avatar_create():
 @app.route('/game/game_screen/<id>')
 @login_required
 def game_screen(id):
-
     specific_recipe = load_specific_recipe(id)
     print(specific_recipe)
 
