b68c6e59627512650936f7caa10a9514c89eafc6
Ashley2n <a.windbreacker@gmail.com>
2025-11-10 14:27:03 -0600

    Working on Profile

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def login():
                 if decrypted_password == password_input:
                     login_user(user_exist)
                     flash("You Have been Logged In", "success")
-                    return redirect(url_for("game_dashboard"))
+                    return redirect(url_for("home"))
             flash("Incorrect email or Password", "danger")
     return render_template('auth/login.html', form=form)
 
@@ -90,7 +90,7 @@ def register():
 
 
         flash("You have been registered", "success")
-        return redirect(url_for('game_dashboard'))
+        return redirect(url_for('home'))
     return render_template('auth/register.html', form=form)
 
 @app.route('/logout')
@@ -99,6 +99,11 @@ def logout():
     session.clear()
     flash("You have been logged out", "success")
     return redirect(url_for('home'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    render_template('profile/index.html', user=get_user_by_id_ser(current_user.id))
 
 
 @app.route('/compare_images/<int:cuisine_id>', methods=['GET', 'POST'])
@@ -130,39 +135,6 @@ def upload_page(cuisine_id):
     return render_template('game/upload.html', form=form, results=results, similarity=similarity)
 
 
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     if 'image' not in request.files:
-#         return "No file uploaded", 400
-#
-#     file = request.files['image']
-#     if file.filename == '':
-#         return "No selected file", 400
-#
-#     image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#     file.save(image_path)
-#
-#     results = analyze_image(image_path)
-#
-#     return render_template('result.html', results=results, filename=file.filename)
-#
-# @app.route('/upload')
-# def upload():
-#     return render_template('game/uploadtest.html')
-# @app.route('/compare', methods=['POST'])
-# def compare():
-#     img1 = request.files['image1']
-#     img2 = request.files['image2']
-#
-#     path1 = os.path.join(app.config['UPLOAD_FOLDER'], img1.filename)
-#     path2 = os.path.join(app.config['UPLOAD_FOLDER'], img2.filename)
-#
-#     img1.save(path1)
-#     img2.save(path2)
-#
-#     similarity = compare_images(path1, path2)
-#
-#     return render_template('result.html', similarity=similarity)
 
 @csrf.exempt
 @app.route('/save-avatar', methods=['POST'])
